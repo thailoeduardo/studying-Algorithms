27d665d2123c8765c849ee80a00746d9a9e0038c
Thailo Dutra <thailo@agenciamobidick.com.br>
2024-02-20 22:19:19 -0300

    hash table exercises

---
+++ hashTables.js
@@ -0,0 +1,71 @@
+/**
+ * Tabela Hash - lista telefonica
+ * 
+ * @see https://www.freecodecamp.org/news/javascript-hash-table-associative-array-hashing-in-js/
+ */
+// const phoneBook = new Map();
+
+// phoneBook.set('Bruno', '1111111');
+// phoneBook.set('Maria', '5555555');
+// phoneBook.set('João', '7777777');
+
+// console.log(phoneBook.get('Maria'));
+
+
+/**
+ * Tabela Hash - Validar se a pessoa já votou
+ * 
+ * @see https://www.freecodecamp.org/news/javascript-hash-table-associative-array-hashing-in-js/
+ */
+
+// list peoples voteds
+// const voted = new Map();
+
+// // Function check people voted
+// function checkVoter(name = '') {
+// 	if( voted.has( name ) ) {
+// 		return 'kick them out!';
+// 	} else {
+// 		voted.set( name, true )
+
+// 		return 'let them vote!';
+// 	}
+// }
+
+// // people included
+// voted.set( 'Thailo', true );
+// voted.set( 'Maria', true );
+// voted.set( 'Pedro', true );
+// voted.set( 'João', true );
+
+// console.log( checkVoter( 'Thailo' ) );
+// console.log( checkVoter( 'Tiago' ) );
+// console.log( checkVoter( 'Fabio' ) );
+// console.log( checkVoter( 'Pedro' ) );
+
+// console.log( voted );
+
+/**
+ * Retorne '1' para qualquer entrada.
+ * 
+ */
+// const people = new Map();
+
+// function itemAlways1(item) {
+// 	if( !people.has(item) ) {
+// 		people.set(item, 1);
+// 	} else {
+// 		people.set(item, 1);
+// 	}
+
+// 	return people;
+// }
+
+// console.log( itemAlways1('thailo') );
+// console.log( itemAlways1('thailo1') );
+// console.log( itemAlways1('thailo2') );
+// console.log( itemAlways1('thailo3') );
+// console.log( itemAlways1('thailo4') );
+// console.log( itemAlways1('thailo5') );
+
+
